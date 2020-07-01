10cd527911b8237d5874d5d1ff4557871c4335c8
Daniele <danielefontana90@gmail.com>
2020-07-01 16:26:48 +0200

    Boolflix keypress

--- script.js
+++ script.js
@@ -2,14 +2,29 @@ $(document).ready(function(){
 
   // CLICK
   $(document).on("click", "#btn-ricerca",function() {
-
+    // svuoto li generata
+    $("#stampa-ricerca").text("");
     var valoreInput = $("#ricerca-film").val();
     stampaFilm(valoreInput);
+    // svuoto la input
+    $("#ricerca-film").val("");
+
   });
 
-    // FUNZIONE STAMPAFILM
+  //KEYPRESS INVIO
+  $("#ricerca-film").keypress(function(event){
+    if (event.which===13 || event.keycode===13) {
+      $("#stampa-ricerca").text("");
+      var valoreInput = $("#ricerca-film").val();
+      stampaFilm(valoreInput);
+      // svuoto la input
+      $("#ricerca-film").val("");
+    }
+  });
 
-    function stampaFilm(arrayRicerca){
+  // FUNZIONE STAMPAFILM
+
+    function stampaFilm(queryRicerca){
 
       // CHIAMATA AJAX
       $.ajax({
@@ -17,65 +32,48 @@ $(document).ready(function(){
         method:"GET",
         data:{
           api_key:"3d9bfe6a800fbc971aaeadf6850c76f4", // <== Chiave API
-          query: arrayRicerca,
+          query: queryRicerca,
           language:"it-IT"   // <== lingua
         },
         success: function(data){
 
-            // handlebars setup
-            var source = $("#film-template").html();
-            var template = Handlebars.compile(source);
+          // handlebars setup
+          var source = $("#film-template").html();
+          var template = Handlebars.compile(source);
 
-            // restituisce l'array della ricerca(query)
-            var arrayRicerca = data.results;
+          // restituisce l'array della ricerca(query)
+          var arrayRicerca = data.results;
 
-            for (var i = 0; i < arrayRicerca.length; i++) {
+          for (var i = 0; i < arrayRicerca.length; i++) {
 
-              // singolofilm
-              var filmSingolo = arrayRicerca[i];
-              //  titolo film
-              var titoloFilm = filmSingolo.title;
-              var titoloOriginale = filmSingolo.original_title;
-              var linguaOriginale = filmSingolo.original_language;
-              var rating = filmSingolo.vote_average;
+            // singolofilm
+            var filmSingolo = arrayRicerca[i];
+            //  titolo film
+            var titoloFilm = filmSingolo.title;
+            var titoloOriginale = filmSingolo.original_title;
+            var linguaOriginale = filmSingolo.original_language;
+            var rating = filmSingolo.vote_average;
 
-              // handlebars
-              var context = {
-                title: titoloFilm,
-                original_title: titoloOriginale,
-                original_language: linguaOriginale,
-                vote_average: rating
-              };
-              var html = template(context);
+            // handlebars
+            var context = {
+              title: titoloFilm,
+              original_title: titoloOriginale,
+              original_language: linguaOriginale,
+              vote_average: rating
+            };
+            var html = template(context);
 
-              $("#stampa-ricerca").append(html);
-            }
+            // APPENDO I FILM
+            $("#stampa-ricerca").append(html);
 
-
-
+          }
         },
         error: function(data){
           alert("ERRORE");
         }
       });
-
     };
 
-    // FINE FUNZIONE STAMPAFILM
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // FINE FUNZIONE STAMPAFILM
 
 });
